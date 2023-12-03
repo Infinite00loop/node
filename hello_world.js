bdcab9851b21544d90229b7bb99a1d5ea2d0f44f
Infinite00loop <samriddhisingh07092002@gmail.com>
2023-12-03 09:11:45 +0530

    request response

--- hello_world.js
+++ hello_world.js
@@ -1,6 +1,22 @@
 // console.log("Hello world")
 const http=require('http');
 const server=http.createServer((req,res)=>{
-    console.log('samriddhi')
+    // console.log('samriddhi')
+    const url=req.url;
+    console.log(url);
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<head><title>My Page</title></head>');
+    if(url==='/home'){
+        res.write('<body><h1>Welcome Home</h1></body>');
+    }
+    else if(url==='/about'){
+         res.write('<body><h1>Welcome to About Us page</h1></body>');
+    }
+    else if(url==='/node'){
+         res.write('<body><h1>Welcome to my Node js Project</h1></body>');
+    }
+    res.write('</html>');
+    res.end();
 });
 server.listen(4000);
