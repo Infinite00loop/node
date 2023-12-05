c7564f44b81c73f54c4765b98a34518f2e9bc519
Infinite00loop <samriddhisingh07092002@gmail.com>
2023-12-05 22:21:42 +0530

    setting up debugger and its autoreloading

--- hello_world.js
+++ hello_world.js
@@ -2,7 +2,7 @@
 const http=require('http');
 
 const xyz = require('./xyz');
-const server=http.createServer(xyz);
+const server=http.createServer(xyz.handler);
     
         
 server.listen(4000);
--- xyz.js
+++ xyz.js
@@ -11,7 +11,7 @@ const requestHandler=(req,res)=>{
         });
         return req.on('end' ,()=>{
             const parsedBody=Buffer.concat(body).toString();
-            const message= parsedBody.split('=')[1];
+            const message=  parsedBody.split('=')[0];
             console.log(message);
             fs.writeFile('message.txt' ,message ,err=>{
                 res.statusCode=302;
@@ -23,7 +23,7 @@ const requestHandler=(req,res)=>{
         res.setHeader('Content-type','text/html');
         res.write('<html>');
         res.write('<head><title>Enter Messaage</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Submit</button></form></body>');
         res.write('</html>');
         return res.end();
 };
