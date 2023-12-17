c830a364096a11c95cc132185d0cc03892530ad8
Infinite00loop <samriddhisingh07092002@gmail.com>
2023-12-17 12:44:47 +0530

    express middleware

--- hello_world.js
+++ hello_world.js
@@ -1,8 +1,21 @@
 
 const http=require('http');
 
-const xyz = require('./xyz');
-const server=http.createServer(xyz.handler);
+const express = require('express');
+
+const app=express();
+
+app.use((req, res, next)=>{
+    console.log('In the middleware');
+    next();
+});
+
+app.use((req,res,next)=>{
+    console.log('In another middleware');
+    res.send('<h1>Hello from Express</h1>')
+});
+
+// const server= http.createServer(app);
     
         
-server.listen(4000);
+app.listen(4000);
