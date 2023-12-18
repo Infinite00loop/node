d76c44037dce598337cd601d27141f320360ab46
Infinite00loop <samriddhisingh07092002@gmail.com>
2023-12-18 10:10:58 +0530

    middleware routing

--- hello_world.js
+++ hello_world.js
@@ -4,16 +4,27 @@ const http=require('http');
 const express = require('express');
 
 const app=express();
+const bodyParser= require('body-parser');
 
-app.use((req, res, next)=>{
-    console.log('In the middleware');
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/',(req,res,next)=>{
+    console.log('This always runs');
     next();
 });
 
-app.use((req,res,next)=>{
-    console.log('In another middleware');
-    res.send('<h1>Hello from Express</h1>')
+app.use('/add-product' ,(req, res, next)=>{
+    console.log('In the middleware');
+    res.send('<form action="/product" method="POST" ><label>Product</label><input type="text" name="Product"><label>Size</label><input type="text" name="size"><button type="submit">Add</button></form>')
+
 });
+
+app.use('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+});
+
+
 
 // const server= http.createServer(app);
     
