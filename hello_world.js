74a0415f771270232eff4d9b6785515cfe914dc0
Infinite00loop <samriddhisingh07092002@gmail.com>
2023-12-22 09:04:53 +0530

    routers and filters

--- hello_world.js
+++ hello_world.js
@@ -2,38 +2,21 @@
 const http=require('http');
 
 const express = require('express');
+const bodyParser=require('body-parser');
 
 const app=express();
-const bodyParser= require('body-parser');
+
+const adminRoutes=require('./routes/admin');
+const shopRoutes=require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use('/',(req,res,next)=>{
-    console.log('This always runs');
-    next();
+app.use('/admin',adminRoutes);
+app.use('/shop',shopRoutes);
+
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page not found</h1>');
 });
-
-app.use('/add-product' ,(req, res, next)=>{
-    console.log('In the middleware');
-    res.send('<form action="/product" method="POST" ><label>Product</label><input type="text" name="Product"><label>Size</label><input type="text" name="size"><button type="submit">Add</button></form>')
-
-});
-
-app.use('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/');
-});
-app.use('/',(req,res,next)=>{
-    console.log('in another middleware');
-    res.send('<h1>hello from Express</h1>');
-    
-});
-
-
-
-
-
-// const server= http.createServer(app);
     
         
 app.listen(4000);
